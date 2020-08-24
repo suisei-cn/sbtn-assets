3255f6ba664d43594250b93d3aff2378e01855ac
Hoshimachiyuri <39732771+Hoshimachiyuri@users.noreply.github.com>
2020-08-24 18:11:19 +0800

    feat: utils/bitrate.ps1

---
+++ utils/bitrate.ps1
@@ -0,0 +1,31 @@
+$TARGET_BITRATE = 200 # Target bitrate limit
+$HAVE_HI_BIT = 0
+if (Test-Path -Path assets) {
+    Set-Location assets
+}
+else {
+    Write-Output "Can't get location. Please switch to root of repository."
+}
+
+$files = Get-Item *.mp3
+foreach ($file in $files) {
+    $null = $filesize = Get-ChildItem | ForEach-Object { [math]::ceiling($file.length / 1kb) }
+    if ($filesize -gt '50') {
+        # Only check files with size > 50kb
+        $info = &"ffprobe" -v error -show_entries format=bit_rate -i $file.Name
+        $BITRATE = ForEach-Object { [math]::ceiling(($info -split "[=]")[2] / 1kb) }
+        if ($BITRATE -gt $TARGET_BITRATE) {
+            if ($args[0] -ne "-o") {
+                # Arguments:
+                #   -o: Only output audio filenames
+                Write-Output "Too high bitrate for '$file' : $BITRATE kbps"
+                $HAVE_HI_BIT = $HAVE_HI_BIT + 1
+            }
+            else {
+                Write-Output $file
+            }
+        }
+    }
+}
+Set-Location ..
+exit $HAVE_HI_BIT
