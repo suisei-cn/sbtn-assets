4af620d2c7fdf74f93fae4aaa87aabaf48bb8419
Outvi V <git@outv.im>
2020-08-24 18:12:51 +0800

    feat: voices.js

---
+++ utils/voices.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const yaml = require("yaml");
+
+function validateFile(filename, paths = "assets/") {
+  if (!fs.existsSync(path.join(paths, filename))) {
+    console.log(`Sound ${filename} doesn't exist!`);
+    process.exit(1);
+  }
+}
+
+console.log("Validating sounds.json...");
+const yamlText = fs.readFileSync("sounds.yml", "utf-8");
+const yamlObj = yaml.parse(yamlText);
+for (const i of yamlObj) {
+  if (typeof i.file === "string") {
+    // Single file
+    validateFile(i.file);
+  } else {
+    // Array of file
+    for (const j of i.file) {
+      validateFile(j);
+    }
+  }
+}
+
+console.log("Successfully validated.")
