35d20a0df716486c280b35d48e89a41f8b01a6b4
Hoshimachiyuri <39732771+Hoshimachiyuri@users.noreply.github.com>
2020-08-24 18:12:18 +0800

    feat: utils/normalize.ps1
    Co-Authored-By: Outvi V <git@outv.im>

---
+++ utils/normalize.ps1
@@ -0,0 +1,13 @@
+if (Test-Path -Path assets) {
+        Set-Location
+} else {
+        Write-Output "Can't get location. Please switch to root of repository."
+}
+$FILENAMES = &"utils\bitrate.ps1" -o
+Set-Location assets
+foreach ($i in $FILENAMES){
+    Rename-Item $i -NewName 'current.mp3'
+    &"ffmpeg" -i current.mp3 -ab 192k -af loudnorm=I=-16:TP=-2:LRA=11 -map_metadata -1 $i
+    Remove-Item current.mp3
+}
+Set-Location ..
