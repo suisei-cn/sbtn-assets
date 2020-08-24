7786421b49706c7451f5ee061d0936fc5ca1b4fd
Outvi V <git@outv.im>
2020-08-24 18:13:23 +0800

    feat: utils/bitrate.sh
    Co-Authored-By: Hoshimachiyuri <39732771+Hoshimachiyuri@users.noreply.github.com>

---
+++ utils/bitrate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Arguments:
+#   -o: Only output audio filenames
+
+# Only check files with size > 50kb
+FILTER_FILE_SIZE=50k
+# Target bitrate limit
+TARGET_BITRATE=200
+
+cd assets
+HAVE_HI_BIT=0
+for i in $(find . -size +$FILTER_FILE_SIZE | grep -v README.md); do
+    BITRATE=$(($(ffprobe -v error -show_entries format=bit_rate -i $i | sed -n "2p" | awk -F '[=]' '{print $2}') / 1024))
+    if [ "$BITRATE" -gt "$TARGET_BITRATE" ]; then
+        if [ "$1" != "-o" ]; then
+            echo "Too high bitrate for '$i' : $BITRATE kbps"
+            HAVE_HI_BIT=$((HAVE_HI_BIT + 1))
+        else
+            echo $i
+        fi
+    fi
+done
+cd ../..
+exit $HAVE_HI_BIT
