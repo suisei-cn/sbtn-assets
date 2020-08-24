6011e1d545eab7e2561ee5e3bd4c55fcabd5cb63
Outvi V <git@outv.im>
2020-08-24 20:24:57 +0800

    lint:

--- utils/voices.js
+++ utils/voices.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
-const yaml = require("yaml");
+const fs = require('fs')
+const path = require('path')
+const yaml = require('yaml')
 
-function validateFile(filename, paths = "assets/") {
+function validateFile(filename, paths = 'assets/') {
   if (!fs.existsSync(path.join(paths, filename))) {
-    console.log(`Sound ${filename} doesn't exist!`);
-    process.exit(1);
+    console.log(`Sound ${filename} doesn't exist!`)
+    process.exit(1)
   }
 }
 
-console.log("Validating sounds.json...");
-const yamlText = fs.readFileSync("sounds.yml", "utf-8");
-const yamlObj = yaml.parse(yamlText);
+console.log('Validating sounds.yml...')
+const yamlText = fs.readFileSync('sounds.yml', 'utf-8')
+const yamlObj = yaml.parse(yamlText)
 for (const i of yamlObj) {
-  if (typeof i.file === "string") {
+  if (typeof i.file === 'string') {
     // Single file
-    validateFile(i.file);
+    validateFile(i.file)
   } else {
     // Array of file
     for (const j of i.file) {
-      validateFile(j);
+      validateFile(j)
     }
   }
 }
 
-console.log("Successfully validated.")
+console.log('Successfully validated.')
