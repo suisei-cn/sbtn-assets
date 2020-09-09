e0f2077e8002b2857c737ada6f6ad8f1c5bfd1be
Outvi V <git@outv.im>
2020-09-09 21:48:58 +0800

    fix: Fix directory

--- utils/normalize.sh
+++ utils/normalize.sh
@@ -2,7 +2,7 @@
 set -e
 
 FILENAMES=$(utils/bitrate.sh -o)
-cd public/assets
+cd assets
 for i in $(echo "$FILENAMES" | tr ' ' ' '); do
     mv $i current.mp3
     ffmpeg -i current.mp3 -ab 192k -af loudnorm=I=-16:TP=-2:LRA=11 -map_metadata -1 $i
