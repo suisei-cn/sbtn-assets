10a9f68dfcc62b3b691d1b4c7d4dadae98ea879f
Outvi V <git@outv.im>
2021-05-02 17:00:51 +0800

    feat: linting bitrate.sh

--- utils/bitrate.sh
+++ utils/bitrate.sh
@@ -7,16 +7,17 @@ FILTER_FILE_SIZE=50k
 # Target bitrate limit
 TARGET_BITRATE=200
 
-cd assets
+cd assets || exit
 HAVE_HI_BIT=0
 for i in $(find . -size +$FILTER_FILE_SIZE | grep -v README.md); do
-    BITRATE=$(($(ffprobe -v error -show_entries format=bit_rate -i $i | sed -n "2p" | awk -F '[=]' '{print $2}') / 1024))
+    BITRATE_VALUE=$(ffprobe -v error -show_entries format=bit_rate -i "$i" | sed -n "2p" | awk -F '[=]' '{print $2}')
+    BITRATE=$((BITRATE_VALUE / 1024))
     if [ "$BITRATE" -gt "$TARGET_BITRATE" ]; then
         if [ "$1" != "-o" ]; then
             echo "Too high bitrate for '$i' : $BITRATE kbps"
             HAVE_HI_BIT=$((HAVE_HI_BIT + 1))
         else
-            echo $i
+            echo "$i"
         fi
     fi
 done
